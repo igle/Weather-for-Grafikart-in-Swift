542038091d653424610d380185254365f6e74bb7
Jules Dubois <dubsjules@gmail.com>
2021-06-27 23:45:47 +0200

    WeatherDetail
    Détail de la météo

--- Weather/MVC/Controller/WeatherDetailViewController.swift
+++ Weather/MVC/Controller/WeatherDetailViewController.swift
@@ -8,22 +8,73 @@
 import UIKit
 
 class WeatherDetailViewController: UIViewController {
-
+    
+    // MARK: - Initialisation des variables
+    var weather : ModelWeather!
+    
+    
+    // MARK: -Initialisation des vues
+    lazy var tableView : UITableView = {
+        let tv : UITableView = UITableView(frame: .zero)
+        tv.register(SimpleWeatherTableViewCell.self, forCellReuseIdentifier: SimpleWeatherTableViewCell.reuseIdentifier)
+        tv.dataSource = self
+        tv.delegate = self
+        tv.estimatedRowHeight = UITableView.automaticDimension
+        tv.rowHeight = UITableView.automaticDimension
+        tv.layoutSubviews()
+        tv.translatesAutoresizingMaskIntoConstraints = false
+        return tv
+    }()
+    
+    
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // On ajoute la tableView au controller
+        view.addSubview(tableView)
     }
     
+    
+    // MARK: - Ajout des contraintes
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        NSLayoutConstraint.activate([
+            
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            
+        ])
+        
     }
-    */
+    
+}
 
+
+// MARK: - tableView delegate
+extension WeatherDetailViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return weather.list.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: SimpleWeatherTableViewCell.reuseIdentifier, for: indexPath) as! SimpleWeatherTableViewCell
+        cell.weatherList = weather.list[indexPath.row]
+        return cell
+    }
+
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
 }
