1cba0e537bafba8f095a9a8c89c5e8ec54949f75
Jules Dubois <dubsjules@gmail.com>
2021-06-27 23:46:19 +0200

    CellSimpleWeather
    La cellule de la table view du détail

---
+++ Weather/MVC/View/Cell/SimpleWeatherTableViewCell.swift
@@ -0,0 +1,80 @@
+//
+//  SimpleWeatherTableViewCell.swift
+//  Weather
+//
+//  Created by Jules Dubois on 27/06/2021.
+//
+
+import UIKit
+
+class SimpleWeatherTableViewCell: UITableViewCell {
+
+    // MARK: - Initialisation variable
+    static let reuseIdentifier: String = "CellSimpleWeather"
+    
+    
+    // MARK: -Initialisation views
+    var labelDateText : TitleLeftLabel = TitleLeftLabel(frame: .zero, title: "")
+    var labelDate : TitleLeftLabel = TitleLeftLabel(frame: .zero, title: "")
+    var labelTemperature : TitleRightLabel = TitleRightLabel(frame: .zero, title: "")
+    
+    
+    // MARK: -Setup data
+    var weatherList: List! {
+        didSet {
+            labelDateText.text = convertTimeStamp(timeStamp: weatherList.dt, dateFormatter: "EEE").capitalizingFirstLetter().replacingOccurrences(of: ".", with: "")
+            labelDate.text = convertTimeStamp(timeStamp: weatherList.dt, dateFormatter: "dd/MM")
+            labelTemperature.text = "\(weatherList.temp.day)°C"
+        }
+    }
+    
+    
+    // MARK: -Init frame
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // Ajout des labels
+        addSubview(labelDateText)
+        labelDate.textColor = UIColor.colorSecondary()
+        addSubview(labelDate)
+        addSubview(labelTemperature)
+        
+        
+        // Change type cell
+        accessoryType = .none
+        selectionStyle = .none
+        
+        // Ajout une constraint
+        NSLayoutConstraint.activate([
+            labelDateText.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 5),
+            labelDateText.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            labelDateText.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor, constant: -5),
+            
+            labelDate.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 5),
+            labelDate.leadingAnchor.constraint(equalTo: labelDateText.trailingAnchor, constant: 10),
+            labelDate.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor, constant: -5),
+            
+            labelTemperature.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 5),
+            labelTemperature.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
+            labelTemperature.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor, constant: -5),
+            
+        ])
+    }
+    
+    
+    // Convertir le timeStamp en date
+    func convertTimeStamp(timeStamp: Int, dateFormatter: String) -> String {
+        let date = NSDate(timeIntervalSince1970: TimeInterval(timeStamp))
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormatter
+        formatter.locale = Locale(identifier: "FR-fr")
+        return formatter.string(from: date as Date)
+    }
+    
+    
+    // MARK: - Init coder
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
